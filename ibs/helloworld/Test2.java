8d388560c818353601cf529a6e734e02017a964c
Кристина <TarasovaKristinaY@yandex.ru>
2023-07-11 12:12:24 +0700

    Commit Test2.java

---
+++ ibs/helloworld/Test2.java
@@ -0,0 +1,9 @@
+package ibs.helloworld;
+
+public class Test2 {
+    public static void main(String[] args) {
+        double a = 2.5753;
+        byte b = 127;
+        System.out.printf("Переменная с плавающей точкой: %f \nПеременная с целочисленным типом: %d", a, b);
+    }
+}
