8605eb286e09ae51e78618e129e71f7e34032855
Кристина <TarasovaKristinaY@yandex.ru>
2023-07-12 18:13:19 +0700

    Add Test5.java

---
+++ ibs/helloworld/Test5.java
@@ -0,0 +1,26 @@
+package ibs.helloworld;
+
+import java.util.Scanner;
+
+public class Test5 {
+    public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите два числа: ");
+            double x = in.nextDouble();
+            double y = in.nextDouble();
+            System.out.println("Введите один из символов ‘+’, ‘-’, ‘*’, ‘/’: ");
+            String z = in.next();
+            double ans = 0;
+            switch (z) {
+                case "+" : ans = x + y;
+                    break;
+                case "-" : ans = x - y;
+                    break;
+                case "*" : ans = x * y;
+                    break;
+                case "/": ans = x / y;
+                    break;
+            }
+            System.out.println("Средне арифметическое число введенных чисел: " + ans);
+        }
+    }
